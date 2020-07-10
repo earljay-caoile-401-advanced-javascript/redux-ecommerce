244d8cc2267dbf9e8790a2a46bf8dc33ec941c1f
Earl Jay Caoile <unfie555@gmail.com>
2020-07-10 10:32:55 -0700

    added header and footer tests

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Header from '../components/Header';
+import { Provider } from 'react-redux';
+import store from '../store';
+
+describe('header component', () => {
+  it('displays the proper html and text on render', () => {
+    const component = mount(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+    expect(component).toBeDefined();
+
+    const h6Tag = component.find('h6');
+    expect(h6Tag).toBeDefined();
+    expect(h6Tag.text()).toBe('Dat Online Store');
+  });
+});
