e4c7a46f3bd21c4cf48edc186a3ab27d71cc4d25
Earl Jay Caoile <unfie555@gmail.com>
2020-07-21 23:17:03 -0700

    added browser router and created details route

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Categories from './components/Categories';
 import ActiveCategory from './components/ActiveCategory';
 import Products from './components/Products';
 import './styles/base.scss';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 /**
  * main component that holds the 3 main components of the app: Header, ToDo, and Footer
@@ -22,11 +23,18 @@ function App() {
   return (
     <Provider store={store}>
       <Header />
-      <div id="main-content" className="fade-in">
-        <Categories />
-        <ActiveCategory />
-        <Products />
-      </div>
+      <BrowserRouter>
+        <div id="main-content" className="fade-in">
+          <Route path="/" exact>
+            <Categories />
+            <ActiveCategory />
+            <Products />
+          </Route>
+          <Route path="/details/:id">
+            <p>Details Page</p>
+          </Route>
+        </div>
+      </BrowserRouter>
       <Footer />
     </Provider>
   );
